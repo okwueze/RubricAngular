b4572ab3e55a8aeba5a25426ca8bbb176a80b409
okwueze <69356352+okwueze@users.noreply.github.com>
2020-09-16 23:01:17 +0100

    changes

--- RubricAngular/src/app/app-routing.module.ts
+++ RubricAngular/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiscussionComponent } from './components/discussion/discussion.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dicussion',
+    component: DiscussionComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RubricAngular/src/app/app.module.ts
+++ RubricAngular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DiscussionComponent } from './components/discussion/discussion.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    HomeComponent,
+    NavbarComponent,
+    DiscussionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
